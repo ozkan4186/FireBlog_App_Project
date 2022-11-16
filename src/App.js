d48747df82e610a1118490c65427508dda370e34
özkan demir <ozkanndemir2009@gmail.com>
2022-11-17 00:00:34 +0300

    sec3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
+import "./App.css";
 
-import './App.css';
 
 function App() {
-  return (
+  return(
     <div className="App">
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     </div>
-  );
+  ) 
 }
 
 export default App;
