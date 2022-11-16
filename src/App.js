55d3fbeaacf68e52929b0f33570dd6996f4fc29d
özkan demir <ozkanndemir2009@gmail.com>
2022-11-17 01:00:22 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import AppRouter from "./app-router/AppRouter";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 
 function App() {
   return(
     <div className="App">
-    
+   <AppRouter/>
     </div>
   ) 
 }
--- src/app-router/AppRouter.jsx
+++ src/app-router/AppRouter.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import Navbar from '../components/Navbar'
+import Dashboard from '../pages/Dashboard'
+import Login from '../pages/Login'
+import Register from '../pages/Register'
 
 const AppRouter = () => {
   return (
-    <div>AppRouter</div>
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Dashboard/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+    </div>
   )
 }
 
